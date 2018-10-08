b7da11ad057bd8c15e1fab8e7be6171fe8dfa7b3
Millad Dagdoni <millad.dagdoni@nav.no>
2018-10-08 13:28:19 +0200

    add status endpoint. Fix endpoint url and port.

--- bidrag-dokument-microservice/src/main/java/no/nav/bidrag/dokument/microservice/controller/JournalpostController.java
+++ bidrag-dokument-microservice/src/main/java/no/nav/bidrag/dokument/microservice/controller/JournalpostController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,12 @@ public class JournalpostController {
     @Autowired
     public JournalpostController(JournalpostService journalpostService) {
         this.journalpostService = journalpostService;
+    }
+
+    @GetMapping("/status")
+    public @ResponseBody
+    String get() {
+        return "OK";
     }
 
     @GetMapping("/journalpost/hent/{jid}")
