5b2cff4fd6d3a0d62c86a1373322d97d7f6ffdde
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-02-18 15:59:55 +0100

    fix url.properties

--- src/main/java/no/nav/bidrag/dokument/BidragDokument.java
+++ src/main/java/no/nav/bidrag/dokument/BidragDokument.java
@@ -1,18 +1,14 @@
 package no.nav.bidrag.dokument;
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 
 @SpringBootApplication
-@PropertySource("classpath:url.properties")
 @EnableOIDCTokenValidation(ignore = {"springfox.documentation.swagger.web.ApiResourceController", "org.springframework"})
 public class BidragDokument {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BidragDokument.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BidragDokument.class, args);
+  }
 }
