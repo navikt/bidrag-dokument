4081babafef635d31ee093efee15d1e944969e29
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-02-18 16:34:23 +0100

    RestTemplateLogger

--- src/main/java/no/nav/bidrag/dokument/RestTemplateConfiguration.java
+++ src/main/java/no/nav/bidrag/dokument/RestTemplateConfiguration.java
@@ -1,14 +1,48 @@
 package no.nav.bidrag.dokument;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(RestTemplateConfiguration.class);
+
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
+    return new RestTemplateLogger();
+  }
+
+  private static class RestTemplateLogger extends RestTemplate {
+
+    @Override
+    public <T> ResponseEntity<T> exchange(String url, HttpMethod method, HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables)
+        throws RestClientException {
+      return logExchange(url, () -> super.exchange(url, method, requestEntity, responseType, uriVariables));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> ResponseEntity<T> logExchange(String url, RestCaller restCaller) {
+      try {
+        return (ResponseEntity<T>) restCaller.doRestApi();
+      } catch (RuntimeException e) {
+        LOGGER.error("Failed to execute rest api - {}, {}: {}", this.getUriTemplateHandler(),  url, e);
+
+        throw e;
+      }
+    }
+
+    @FunctionalInterface
+    private interface RestCaller {
+
+      Object doRestApi();
+    }
   }
 }
