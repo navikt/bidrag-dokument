39e28f2ed1be497b22f931e61d565d489dc7a18f
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-03-07 09:47:11 +0100

    felles HttpHeaderRestTemplate

--- src/main/java/no/nav/bidrag/dokument/RestTemplateConfiguration.java
+++ src/main/java/no/nav/bidrag/dokument/RestTemplateConfiguration.java
@@ -3,8 +3,7 @@ package no.nav.bidrag.dokument;
 import static no.nav.bidrag.dokument.BidragDokumentConfig.ISSUER;
 
 import java.util.Optional;
-import no.nav.bidrag.dokument.HttpHeaderRestTemplate.Header;
-import no.nav.bidrag.dokument.HttpHeaderRestTemplate.HeaderGenerator;
+import no.nav.bidrag.commons.web.HttpHeaderRestTemplate;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.security.oidc.context.TokenContext;
 import org.springframework.context.annotation.Bean;
@@ -20,31 +19,18 @@ public class RestTemplateConfiguration {
   @Scope("prototype")
   public RestTemplate restTemplate(OIDCRequestContextHolder oidcRequestContextHolder) {
     HttpHeaderRestTemplate httpHeaderRestTemplate = new HttpHeaderRestTemplate();
-    httpHeaderRestTemplate.addHeaderGenerator(createBearerTokenGenerator(oidcRequestContextHolder));
+    httpHeaderRestTemplate.addHeaderGenerator(
+        () -> new HttpHeaderRestTemplate.Header(HttpHeaders.AUTHORIZATION, () -> "Bearer " + fetchBearerToken(oidcRequestContextHolder))
+    );
 
     return httpHeaderRestTemplate;
   }
 
-  private HeaderGenerator createBearerTokenGenerator(OIDCRequestContextHolder oidcRequestContextHolder) {
-    return () -> new Header() {
-
-      @Override
-      public String name() {
-        return HttpHeaders.AUTHORIZATION;
-      }
-
-      @Override
-      public String value() {
-        return "Bearer " + fetchBearerToken();
-      }
-
-      private String fetchBearerToken() {
-        return Optional.ofNullable(oidcRequestContextHolder)
-            .map(OIDCRequestContextHolder::getOIDCValidationContext)
-            .map(oidcValidationContext -> oidcValidationContext.getToken(ISSUER))
-            .map(TokenContext::getIdToken)
-            .orElseThrow(() -> new IllegalStateException("Kunne ikke videresende Bearer token"));
-      }
-    };
+  private String fetchBearerToken(OIDCRequestContextHolder oidcRequestContextHolder) {
+    return Optional.ofNullable(oidcRequestContextHolder)
+        .map(OIDCRequestContextHolder::getOIDCValidationContext)
+        .map(oidcValidationContext -> oidcValidationContext.getToken(ISSUER))
+        .map(TokenContext::getIdToken)
+        .orElseThrow(() -> new IllegalStateException("Kunne ikke videresende Bearer token"));
   }
 }
