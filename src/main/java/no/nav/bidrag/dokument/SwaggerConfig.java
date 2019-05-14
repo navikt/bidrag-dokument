7d429a6877ed03cb70826fce9649bf783a8c38c8
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-05-14 09:59:57 +0200

    filter out guava dependency - only visible through springfox

--- src/main/java/no/nav/bidrag/dokument/SwaggerConfig.java
+++ src/main/java/no/nav/bidrag/dokument/SwaggerConfig.java
@@ -1,12 +1,8 @@
 package no.nav.bidrag.dokument;
 
-import static com.google.common.collect.Lists.newArrayList;
-
 import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
@@ -21,33 +17,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(BidragDokument.class.getPackage().getName()))
-                .build()
-                .securitySchemes(newArrayList(apiKey()))
-                .securityContexts(newArrayList(securityContext()));
-    }
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage(BidragDokument.class.getPackage().getName()))
+        .build()
+        .securitySchemes(List.of(apiKey()))
+        .securityContexts(List.of(securityContext()));
+  }
 
-    private ApiKey apiKey() {
-        return new ApiKey("mykey", "Authorization", "header");
-    }
+  private ApiKey apiKey() {
+    return new ApiKey("mykey", "Authorization", "header");
+  }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.regex("/*.*"))
-                .build();
-    }
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.regex("/*.*"))
+        .build();
+  }
 
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return newArrayList(
-                new SecurityReference("mykey", authorizationScopes));
-    }
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+
+    return List.of(new SecurityReference("mykey", authorizationScopes));
+  }
 
 }
