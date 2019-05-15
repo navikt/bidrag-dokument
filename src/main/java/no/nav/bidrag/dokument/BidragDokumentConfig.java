1a94a504ce502e78dcc1c140309808ee2cada1dc
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-05-15 15:27:39 +0200

    reformatted

--- src/main/java/no/nav/bidrag/dokument/BidragDokumentConfig.java
+++ src/main/java/no/nav/bidrag/dokument/BidragDokumentConfig.java
@@ -26,13 +26,14 @@ public class BidragDokumentConfig {
   public static final String PREFIX_BIDRAG = "BID";
   public static final String PREFIX_JOARK = "JOARK";
   public static final String ISSUER = "isso";
-  public static final String LIVE_PROFILE = "live";
+  static final String LIVE_PROFILE = "live";
   public static final String TEST_PROFILE = "test";
   public static final String SECURE_TEST_PROFILE = "secure-test";
 
   @Bean
   public BidragJournalpostConsumer bidragJournalpostConsumer(
-      @Value("${JOURNALPOST_URL}") String journalpostBaseUrl, RestTemplate restTemplate) {
+      @Value("${JOURNALPOST_URL}") String journalpostBaseUrl, RestTemplate restTemplate
+  ) {
     restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(journalpostBaseUrl));
     LOGGER.info("BidragJournalpostConsumer med base url: " + journalpostBaseUrl);
 
@@ -41,7 +42,8 @@ public class BidragDokumentConfig {
 
   @Bean
   public BidragSakConsumer bidragSakConsumer(
-      @Value("${BIDRAG_SAK_URL}") String sakBaseUrl, RestTemplate restTemplate) {
+      @Value("${BIDRAG_SAK_URL}") String sakBaseUrl, RestTemplate restTemplate
+  ) {
     restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(sakBaseUrl));
     LOGGER.info("BidragSakConsumer med base url: " + sakBaseUrl);
 
@@ -50,7 +52,8 @@ public class BidragDokumentConfig {
 
   @Bean
   public BidragArkivConsumer journalforingConsumer(
-      @Value("${BIDRAG_ARKIV_URL}") String bidragArkivBaseUrl, RestTemplate restTemplate) {
+      @Value("${BIDRAG_ARKIV_URL}") String bidragArkivBaseUrl, RestTemplate restTemplate
+  ) {
     restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(bidragArkivBaseUrl));
     LOGGER.info("BidragArkivConsumer med base url: " + bidragArkivBaseUrl);
 
@@ -63,8 +66,9 @@ public class BidragDokumentConfig {
       @Value("${SRVBISYS_USERNAME}") String systemUser,
       @Value("${SRVBISYS_PASSWORD}") String systemPassword,
       OidcTokenManager oidcTokenManager,
-      RestTemplate restTemplate) {
-    restTemplate.setUriTemplateHandler(new RootUriTemplateHandler( securityTokenBaseUrl));
+      RestTemplate restTemplate
+  ) {
+    restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(securityTokenBaseUrl));
     LOGGER.info("SecurityTokenConsumer med base url: " + securityTokenBaseUrl);
 
     return new SecurityTokenConsumer(restTemplate, systemUser, systemPassword, oidcTokenManager);
@@ -91,6 +95,7 @@ public class BidragDokumentConfig {
 
   @FunctionalInterface
   public interface OidcTokenManager {
+
     String fetchToken();
   }
 }
