ed3eaaa306f7fbe74382f801e9c3113fa21c5e99
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-05-16 15:07:20 +0200

    input er DokumentTilgangRequest

--- src/main/java/no/nav/bidrag/dokument/service/DokumentService.java
+++ src/main/java/no/nav/bidrag/dokument/service/DokumentService.java
@@ -2,6 +2,7 @@ package no.nav.bidrag.dokument.service;
 
 import no.nav.bidrag.commons.web.HttpStatusResponse;
 import no.nav.bidrag.dokument.consumer.DokumentConsumer;
+import no.nav.bidrag.dokument.dto.DokumentTilgangRequest;
 import no.nav.bidrag.dokument.dto.DokumentUrlDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class DokumentService {
     this.dokumentConsumer = dokumentConsumer;
   }
 
-  public HttpStatusResponse<DokumentUrlDto> hentTilgangUrl(DokumentUrlDto dokumentUrlDto) {
-    return dokumentConsumer.hentTilgangUrl(dokumentUrlDto);
+  public HttpStatusResponse<DokumentUrlDto> hentTilgangUrl(DokumentTilgangRequest dokumentTilgangRequest) {
+    return dokumentConsumer.hentTilgangUrl(dokumentTilgangRequest);
   }
 }
