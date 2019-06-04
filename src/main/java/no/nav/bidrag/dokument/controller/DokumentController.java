5e52a7cd8ad5583c29a9e36154eb99cdecfabcfb
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-06-04 14:57:38 +0200

    @RequestBody

--- src/main/java/no/nav/bidrag/dokument/controller/DokumentController.java
+++ src/main/java/no/nav/bidrag/dokument/controller/DokumentController.java
@@ -2,7 +2,6 @@ package no.nav.bidrag.dokument.controller;
 
 import static no.nav.bidrag.dokument.BidragDokumentConfig.ISSUER;
 
-import no.nav.bidrag.commons.web.HttpStatusResponse;
 import no.nav.bidrag.dokument.dto.DokumentTilgangRequest;
 import no.nav.bidrag.dokument.dto.DokumentUrlDto;
 import no.nav.bidrag.dokument.service.DokumentService;
@@ -11,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,7 +26,7 @@ public class DokumentController {
   }
 
   @PostMapping("/tilgang/url")
-  public ResponseEntity<DokumentUrlDto> giTilgangTilDokument(DokumentTilgangRequest dokumentTilgangRequest) {
+  public ResponseEntity<DokumentUrlDto> giTilgangTilDokument(@RequestBody DokumentTilgangRequest dokumentTilgangRequest) {
     LOGGER.info("Spør om tilgang til dokument: " + dokumentTilgangRequest);
 
     var dokumentUrlResponse = dokumentService.hentTilgangUrl(dokumentTilgangRequest);
