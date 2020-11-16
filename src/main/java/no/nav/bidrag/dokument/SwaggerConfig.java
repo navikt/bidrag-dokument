073938210f4871f61273f29696ae95f7e8e63b96
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-11-16 16:07:59 +0100

    remove web configurer

--- src/main/java/no/nav/bidrag/dokument/SwaggerConfig.java
+++ src/main/java/no/nav/bidrag/dokument/SwaggerConfig.java
@@ -2,13 +2,9 @@ package no.nav.bidrag.dokument;
 
 import java.util.List;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
@@ -21,7 +17,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
   @Bean
   public Docket api() {
@@ -58,19 +54,5 @@ public class SwaggerConfig implements WebMvcConfigurer {
         "mykey",
         new AuthorizationScope[]{new AuthorizationScope("global", "accessEverything")}
     ));
-  }
-
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
-    registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-    registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-    registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
-  }
-
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/api/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-    registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 }
