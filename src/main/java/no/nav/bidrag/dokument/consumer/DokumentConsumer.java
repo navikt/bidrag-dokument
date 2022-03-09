94ae053abef6fdb5bb2988e08395118f4abace4e
Ugur Cenar <cenar93@gmail.com>
2022-03-09 13:45:49 +0100

    Simplify restemplate config

--- src/main/java/no/nav/bidrag/dokument/consumer/DokumentConsumer.java
+++ src/main/java/no/nav/bidrag/dokument/consumer/DokumentConsumer.java
@@ -3,19 +3,20 @@ package no.nav.bidrag.dokument.consumer;
 import no.nav.bidrag.commons.web.HttpResponse;
 import no.nav.bidrag.dokument.dto.DokumentTilgangResponse;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
 
 public class DokumentConsumer {
 
   public static final String PATH_DOKUMENT_TILGANG = "/tilgang/%s/%s";
 
-  private final ConsumerTarget consumerTarget;
+  private final RestTemplate restTemplate;
 
-  public DokumentConsumer(ConsumerTarget consumerTarget) {
-    this.consumerTarget = consumerTarget;
+  public DokumentConsumer(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
   }
   public HttpResponse<DokumentTilgangResponse> hentTilgangUrl(String journalpostId, String dokumentreferanse) {
 
-    var response = consumerTarget.henteRestTemplateForIssuer()
+    var response = restTemplate
         .exchange(String.format(PATH_DOKUMENT_TILGANG, journalpostId, dokumentreferanse), HttpMethod.GET, null, DokumentTilgangResponse.class);
 
     return new HttpResponse<>(response);
