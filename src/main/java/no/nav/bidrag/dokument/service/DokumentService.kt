9d138b3ddb54d08caa314df10eaeb4f434253b24
Ugur Cenar <cenar93@gmail.com>
2022-12-27 11:03:37 +0100

    Support for forsendelse

--- src/main/java/no/nav/bidrag/dokument/service/DokumentService.kt
+++ src/main/java/no/nav/bidrag/dokument/service/DokumentService.kt
@@ -1,140 +1,127 @@
-package no.nav.bidrag.dokument.service;
+package no.nav.bidrag.dokument.service
 
-import static no.nav.bidrag.dokument.BidragDokumentConfig.ARKIV_QUALIFIER;
-import static no.nav.bidrag.dokument.BidragDokumentConfig.MIDL_BREVLAGER_QUALIFIER;
-import static no.nav.bidrag.dokument.service.PDFDokumentProcessor.fileToByte;
-
-import com.google.common.io.ByteSource;
-import io.micrometer.core.annotation.Timed;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import no.nav.bidrag.commons.web.HttpResponse;
-import no.nav.bidrag.dokument.consumer.BidragDokumentConsumer;
-import no.nav.bidrag.dokument.consumer.DokumentTilgangConsumer;
-import no.nav.bidrag.dokument.dto.DokumentDto;
-import no.nav.bidrag.dokument.dto.DocumentProperties;
-import no.nav.bidrag.dokument.dto.DokumentRef;
-import no.nav.bidrag.dokument.dto.DokumentTilgangResponse;
-import no.nav.bidrag.dokument.dto.JournalpostResponse;
-import no.nav.bidrag.dokument.dto.Kilde;
-import org.apache.pdfbox.io.MemoryUsageSetting;
-import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
+import com.google.common.io.ByteSource
+import io.micrometer.core.annotation.Timed
+import no.nav.bidrag.commons.web.HttpResponse
+import no.nav.bidrag.dokument.BidragDokumentConfig
+import no.nav.bidrag.dokument.consumer.BidragDokumentConsumer
+import no.nav.bidrag.dokument.consumer.DokumentTilgangConsumer
+import no.nav.bidrag.dokument.dto.DocumentProperties
+import no.nav.bidrag.dokument.dto.DokumentDto
+import no.nav.bidrag.dokument.dto.DokumentRef
+import no.nav.bidrag.dokument.dto.DokumentTilgangResponse
+import no.nav.bidrag.dokument.dto.Kilde
+import org.apache.pdfbox.io.MemoryUsageSetting
+import org.apache.pdfbox.multipdf.PDFMergerUtility
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpStatusCodeException
+import java.io.File
+import java.io.IOException
+import java.util.UUID
+import java.util.stream.Collectors
 
 @Service
-public class DokumentService {
-  private static final Logger LOGGER = LoggerFactory.getLogger(DokumentService.class);
-
-  private final JournalpostService journalpostService;
-  private final BidragDokumentConsumer bidragJournalpostConsumer;
-  private final BidragDokumentConsumer bidragArkivConsumer;
-  private final DokumentTilgangConsumer dokumentTilgangConsumer;
-
-  public DokumentService(
-      JournalpostService journalpostService, DokumentTilgangConsumer dokumentTilgangConsumer,
-      @Qualifier(ARKIV_QUALIFIER) BidragDokumentConsumer bidragArkivConsumer,
-      @Qualifier(MIDL_BREVLAGER_QUALIFIER) BidragDokumentConsumer bidragJournalpostConsumer
-  ) {
-    this.journalpostService = journalpostService;
-    this.dokumentTilgangConsumer = dokumentTilgangConsumer;
-    this.bidragArkivConsumer = bidragArkivConsumer;
-    this.bidragJournalpostConsumer = bidragJournalpostConsumer;
-  }
-  public HttpResponse<DokumentTilgangResponse> hentTilgangUrl(String journalpostId, String dokumentreferanse) {
-    return dokumentTilgangConsumer.hentTilgangUrl(journalpostId, dokumentreferanse);
-  }
-
-  @Timed("hentDokument")
-  public ResponseEntity<byte[]> hentDokument(DokumentRef dokumentRef, DocumentProperties documentProperties) {
-    if (!dokumentRef.hasDokumentId() && dokumentRef.erForKilde(Kilde.JOARK)){
-      var dokumentReferanser = hentAlleJournalpostDokumentReferanser(dokumentRef);
-      return hentDokumenterData(dokumentReferanser, documentProperties);
+class DokumentService(
+    private val journalpostService: JournalpostService,
+    private val dokumentTilgangConsumer: DokumentTilgangConsumer,
+    @Qualifier(BidragDokumentConfig.ARKIV_QUALIFIER) private val bidragArkivConsumer: BidragDokumentConsumer,
+    @Qualifier(BidragDokumentConfig.MIDL_BREVLAGER_QUALIFIER) private val bidragJournalpostConsumer: BidragDokumentConsumer,
+    @Qualifier(BidragDokumentConfig.FORSENDELSE_QUALIFIER) private val bidragForsendelseConsumer: BidragDokumentConsumer
+) {
+    fun hentTilgangUrl(journalpostId: String?, dokumentreferanse: String?): HttpResponse<DokumentTilgangResponse> {
+        return dokumentTilgangConsumer.hentTilgangUrl(journalpostId, dokumentreferanse)
     }
 
-    ResponseEntity<byte[]> response = hentDokumentData(dokumentRef);
-
-    return ResponseEntity.ok()
-        .contentType(MediaType.APPLICATION_PDF)
-        .header(HttpHeaders.CONTENT_DISPOSITION, response.getHeaders().getContentDisposition().toString())
-        .body(new PDFDokumentProcessor().process(response.getBody(), documentProperties));
-  }
-
-  @Timed("hentDokumenter")
-  public ResponseEntity<byte[]> hentDokumenter(List<String> dokumenterString, DocumentProperties documentProperties){
-      var dokumenter = parseDokumentString(dokumenterString);
-      return hentDokumenterData(dokumenter, documentProperties);
-  }
-
-  private ResponseEntity<byte[]> hentDokumentData(DokumentRef dokument) {
-    if (dokument.erForKilde(Kilde.BIDRAG)) {
-      return bidragJournalpostConsumer.hentDokument(dokument.getJournalpostId(), dokument.getDokumentId());
+    @Timed("hentDokument")
+    fun hentDokument(dokumentRef: DokumentRef, documentProperties: DocumentProperties): ResponseEntity<ByteArray> {
+        if (!dokumentRef.hasDokumentId() && !dokumentRef.erForKilde(Kilde.BIDRAG)) {
+            val dokumentReferanser = hentAlleJournalpostDokumentReferanser(dokumentRef)
+            return hentDokumenterData(dokumentReferanser, documentProperties)
+        }
+        val response = hentDokumentData(dokumentRef)
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_PDF)
+            .header(HttpHeaders.CONTENT_DISPOSITION, response.headers.contentDisposition.toString())
+            .body(PDFDokumentProcessor().process(response.body, documentProperties))
     }
-    return bidragArkivConsumer.hentDokument(dokument.getJournalpostId(), dokument.getDokumentId());
-  }
 
-  private ResponseEntity<byte[]> hentDokumenterData(List<DokumentRef> dokumentRefList, DocumentProperties documentProperties){
-    try {
-      var dokumentByte = hentOgMergeAlleDokumenter(dokumentRefList, documentProperties);
-      return ResponseEntity.ok()
-          .contentType(MediaType.APPLICATION_PDF)
-          .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=dokumenter_sammenslatt.pdf")
-          .body(dokumentByte);
-    } catch (Exception e){
-      LOGGER.error("Det skjedde en feil ved henting av dokumenter {}", dokumentRefList, e);
-      if (e instanceof HttpStatusCodeException){
-        return ResponseEntity.status(((HttpStatusCodeException) e).getStatusCode()).build();
-      }
-      return ResponseEntity.internalServerError().build();
+    @Timed("hentDokumenter")
+    fun hentDokumenter(dokumenterString: List<String>, documentProperties: DocumentProperties): ResponseEntity<ByteArray> {
+        val dokumenter = parseDokumentString(dokumenterString)
+        return hentDokumenterData(dokumenter, documentProperties)
     }
-  }
 
-  private byte[] hentOgMergeAlleDokumenter(List<DokumentRef> dokumentList, DocumentProperties documentProperties) throws IOException {
-    documentProperties.setNumberOfDocuments(dokumentList.size());
-    if (dokumentList.size() == 1){
-      return hentDokument(dokumentList.get(0), documentProperties).getBody();
+    private fun hentDokumentData(dokument: DokumentRef): ResponseEntity<ByteArray> {
+        return if (dokument.erForKilde(Kilde.BIDRAG)) bidragJournalpostConsumer.hentDokument(dokument.journalpostId, dokument.dokumentId)
+        else if (dokument.erForKilde(Kilde.FORSENDELSE)) bidragForsendelseConsumer.hentDokument(dokument.journalpostId, dokument.dokumentId)
+        else bidragArkivConsumer.hentDokument(dokument.journalpostId, dokument.dokumentId)
     }
-    var mergedFileName = "/tmp/" + UUID.randomUUID();
-    PDFMergerUtility mergedDocument = new PDFMergerUtility();
-    mergedDocument.setDestinationFileName(mergedFileName);
-    for (var dokument: dokumentList){
-      var dokumentResponse = hentDokument(dokument, new DocumentProperties(documentProperties, dokumentList.indexOf(dokument)));
-      var dokumentInputStream = ByteSource.wrap(dokumentResponse.getBody()).openStream();
-      mergedDocument.addSource(dokumentInputStream);
-      dokumentInputStream.close();
+
+    private fun hentDokumenterData(dokumentRefList: List<DokumentRef>, documentProperties: DocumentProperties): ResponseEntity<ByteArray> {
+        return try {
+            val dokumentByte = hentOgMergeAlleDokumenter(dokumentRefList, documentProperties)
+            ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=dokumenter_sammenslatt.pdf")
+                .body(dokumentByte)
+        } catch (e: Exception) {
+            LOGGER.error("Det skjedde en feil ved henting av dokumenter {}", dokumentRefList, e)
+            if (e is HttpStatusCodeException) ResponseEntity.status(e.statusCode).build()
+            else ResponseEntity.internalServerError().build()
+        }
     }
-    mergedDocument.mergeDocuments(MemoryUsageSetting.setupTempFileOnly());
-    return getByteDataAndDeleteFile(mergedFileName);
-  }
 
-  private byte[] getByteDataAndDeleteFile(String filename) throws IOException {
-    var file = new File(filename);
-    try {
-      return fileToByte(new File(filename));
-    } finally {
-      file.delete();
+    @Throws(IOException::class)
+    private fun hentOgMergeAlleDokumenter(dokumentList: List<DokumentRef>, documentProperties: DocumentProperties): ByteArray? {
+        documentProperties.numberOfDocuments = dokumentList.size
+        if (dokumentList.size == 1) {
+            return hentDokument(dokumentList[0], documentProperties).body
+        }
+        val mergedFileName = "/tmp/" + UUID.randomUUID()
+        val mergedDocument = PDFMergerUtility()
+        mergedDocument.destinationFileName = mergedFileName
+        for (dokument in dokumentList) {
+            val dokumentResponse = hentDokument(dokument, DocumentProperties(documentProperties, dokumentList.indexOf(dokument)))
+            val dokumentInputStream = ByteSource.wrap(dokumentResponse.body!!).openStream()
+            mergedDocument.addSource(dokumentInputStream)
+            dokumentInputStream.close()
+        }
+        mergedDocument.mergeDocuments(MemoryUsageSetting.setupTempFileOnly())
+        return getByteDataAndDeleteFile(mergedFileName)
     }
-  }
 
-  private List<DokumentRef> hentAlleJournalpostDokumentReferanser(DokumentRef dokumentRef){
-    JournalpostResponse journalpost = journalpostService.hentJournalpost(dokumentRef, null).fetchBody().get();
-    return journalpost.getJournalpost().getDokumenter()
-        .stream()
-        .map((DokumentDto::getDokumentreferanse))
-        .map((dokumentId)->new DokumentRef(dokumentRef.getJournalpostId(), dokumentId))
-        .collect(Collectors.toList());
-  }
+    @Throws(IOException::class)
+    private fun getByteDataAndDeleteFile(filename: String): ByteArray {
+        val file = File(filename)
+        return try {
+            PDFDokumentProcessor.fileToByte(File(filename))
+        } finally {
+            file.delete()
+        }
+    }
 
-  private List<DokumentRef> parseDokumentString(List<String> dokumenterString){
-    return dokumenterString.stream().map(DokumentRef.Companion::parseFromString).collect(Collectors.toList());
-  }
+    private fun hentAlleJournalpostDokumentReferanser(dokumentRef: DokumentRef): List<DokumentRef> {
+        val (journalpost) = journalpostService.hentJournalpost(dokumentRef, null).fetchBody().get()
+        return journalpost!!.dokumenter
+            .stream()
+            .map { (dokumentreferanse, journalpostId): DokumentDto -> DokumentRef(journalpostId ?: dokumentRef.journalpostId, dokumentreferanse) }
+            .collect(Collectors.toList())
+    }
+
+    private fun parseDokumentString(dokumenterString: List<String>): List<DokumentRef> {
+        return dokumenterString.stream().map { str: String? ->
+            DokumentRef.parseFromString(
+                str!!
+            )
+        }.collect(Collectors.toList())
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(DokumentService::class.java)
+    }
 }
