3f80e456e036a725c3e9d449c19e3e7a7860bc68
Ugur Cenar <cenar93@gmail.com>
2023-01-19 13:42:34 +0100

    Increase memory size

--- src/main/java/no/nav/bidrag/dokument/aop/BidragDokumentRestControllerAdvice.java
+++ src/main/java/no/nav/bidrag/dokument/aop/BidragDokumentRestControllerAdvice.java
@@ -4,19 +4,21 @@ import static no.nav.bidrag.commons.web.WebUtil.initHttpHeadersWith;
 
 import no.nav.bidrag.commons.ExceptionLogger;
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class BidragDokumentRestControllerAdvice extends ResponseEntityExceptionHandler {
+  private static final Logger LOGGER = LoggerFactory.getLogger(BidragDokumentRestControllerAdvice.class);
 
   private final ExceptionLogger exceptionLogger;
 
@@ -27,6 +29,7 @@ public class BidragDokumentRestControllerAdvice extends ResponseEntityExceptionH
   @ResponseBody
   @ExceptionHandler
   public ResponseEntity<?> handleOtherExceptions(Exception exception) {
+    LOGGER.error("Det skjedde en ukjent feil: " + exception.getMessage(), exception);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .header(HttpHeaders.WARNING, "Det skjedde en ukjent feil: " + exception.getMessage())
         .build();
