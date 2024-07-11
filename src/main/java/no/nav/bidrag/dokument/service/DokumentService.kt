0160dff560288734010f81096153de26e9e46138
Ugur Cenar <cenar93@gmail.com>
2024-07-11 20:46:51 +0200

    Hent RTF dokument

--- src/main/java/no/nav/bidrag/dokument/service/DokumentService.kt
+++ src/main/java/no/nav/bidrag/dokument/service/DokumentService.kt
@@ -167,12 +167,12 @@ class DokumentService(
             journalpostId = dokument.journalpostId,
             dokumentId = dokument.dokumentreferanse,
             kilde =
-                when (dokument.arkivsystem) {
-                    DokumentArkivSystemDto.MIDLERTIDLIG_BREVLAGER -> Kilde.MIDLERTIDLIG_BREVLAGER
-                    DokumentArkivSystemDto.JOARK -> Kilde.JOARK
-                    DokumentArkivSystemDto.BIDRAG -> Kilde.FORSENDELSE
-                    else -> null
-                },
+            when (dokument.arkivsystem) {
+                DokumentArkivSystemDto.MIDLERTIDLIG_BREVLAGER -> Kilde.MIDLERTIDLIG_BREVLAGER
+                DokumentArkivSystemDto.JOARK -> Kilde.JOARK
+                DokumentArkivSystemDto.BIDRAG -> Kilde.FORSENDELSE
+                else -> null
+            },
         )
     }
 
